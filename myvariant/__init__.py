77043579544448476ba7c28772c1f0e189bcec95
jschaff <jschaff@scripps.edu>
2025-01-13 10:25:47 -0800

    Re-add the alwayslist method ...
    Was initially removed from biothings-client as nothing
    internally leveraged it. The method is trivial enough
    that I don't think we need to provide it to users, but
    for maintenance purposes I'm providing it in our older
    modules for compatibility

--- myvariant/__init__.py
+++ myvariant/__init__.py
@@ -1,33 +1,68 @@
-'''
+"""
 Python Client for MyVariant.Info services
-'''
-from biothings_client import get_client, alwayslist
+"""
 
-__version__ = '1.0.0'
+from biothings_client import get_client
 
-class MyVariantInfo(get_client('variant', instance=False)):
-    '''This is the client for MyVariant.info web services.
+__version__ = "1.0.0"
+
+# import alwayslist helper function here in case users
+# still use "from mygene import alwayslist"
+
+
+# *** NOTE ***
+# Initially removed from biothings_client as nothing in our core
+# code used. Appears it was used here for external usage. Moving
+# here in case users still utilize it (Jan 13, 2025)
+def alwayslist(value):
+    """If input value if not a list/tuple type, return it as a single value list.
+
+    Example:
+
+    >>> x = 'abc'
+    >>> for xx in alwayslist(x):
+    ...     print xx
+    >>> x = ['abc', 'def']
+    >>> for xx in alwayslist(x):
+    ...     print xx
+
+    """
+    if isinstance(value, (list, tuple)):
+        return value
+    else:
+        return [value]
+
+
+class MyVariantInfo(get_client("variant", instance=False)):
+    """This is the client for MyVariant.info web services.
     Example:
 
         >>> mv = MyVariantInfo()
 
-    '''
+    """
+
     def __init__(self):
         super(MyVariantInfo, self).__init__()
-        self.default_user_agent += ' myvariant.py/' + __version__
+        self.default_user_agent += " myvariant.py/" + __version__
+
 
 def format_hgvs(chrom, pos, ref, alt):
     """:py:func:`format_hgvs` helper function is now moved into :py:class:`MyVariantInfo` class
-       as a method: :py:meth:`format_hgvs`.
+    as a method: :py:meth:`format_hgvs`.
     """
     mv = MyVariantInfo()
     return mv.format_hgvs(chrom=chrom, pos=pos, ref=ref, alt=alt)
+
+
 # format_hgvs.__doc__ = MyVariantInfo.format_hgvs.__doc__
+
 
 def get_hgvs_from_vcf(input_vcf):
     """:py:func:`get_hgvs_from_vcf` helper function is now moved into :py:class:`MyVariantInfo` class
-       as a method: :py:meth:`get_hgvs_from_vcf`.
+    as a method: :py:meth:`get_hgvs_from_vcf`.
     """
     mv = MyVariantInfo()
     return mv.get_hgvs_from_vcf(input_vcf=input_vcf)
+
+
 # get_hgvs_from_vcf.__doc__ = MyVariantInfo.get_hgvs_from_vcf.__doc__
--- tests/test.py
+++ tests/test.py
@@ -2,99 +2,115 @@ import unittest
 import sys
 import os
 import types
+
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 try:
     from pandas import DataFrame
+
     pandas_avail = True
 except ImportError:
     pandas_avail = False
 try:
     import requests_cache
+
     caching_avail = True
 except ImportError:
     caching_avail = False
 sys.path.insert(0, os.path.split(os.path.split(os.path.abspath(__file__))[0])[0])
 
 import myvariant
-sys.stderr.write('"myvariant {0}" loaded from "{1}"\n'.format(myvariant.__version__, myvariant.__file__))
+
+sys.stderr.write(
+    '"myvariant {0}" loaded from "{1}"\n'.format(
+        myvariant.__version__, myvariant.__file__
+    )
+)
+
 
 class TestMyVariantPy(unittest.TestCase):
 
     def setUp(self):
         self.mv = myvariant.MyVariantInfo()
         self.query_list1 = [
-            'chr1:g.866422C>T',
-            'chr1:g.876664G>A',
-            'chr1:g.69635G>C',
-            'chr1:g.69869T>A',
-            'chr1:g.881918G>A',
-            'chr1:g.865625G>A',
-            'chr1:g.69892T>C',
-            'chr1:g.879381C>T',
-            'chr1:g.878330C>G'
+            "chr1:g.866422C>T",
+            "chr1:g.876664G>A",
+            "chr1:g.69635G>C",
+            "chr1:g.69869T>A",
+            "chr1:g.881918G>A",
+            "chr1:g.865625G>A",
+            "chr1:g.69892T>C",
+            "chr1:g.879381C>T",
+            "chr1:g.878330C>G",
         ]
         self.query_list2 = [
-            'rs374802787',
-            'rs1433078',
-            'rs1433115',
-            'rs377266517',
-            'rs587640013',
-            'rs137857980',
-            'rs199710579',
-            'rs186823979',
-            'rs2276240',
-            'rs372452565'
+            "rs374802787",
+            "rs1433078",
+            "rs1433115",
+            "rs377266517",
+            "rs587640013",
+            "rs137857980",
+            "rs199710579",
+            "rs186823979",
+            "rs2276240",
+            "rs372452565",
         ]
 
     def test_format_hgvs(self):
-        self.assertEqual(myvariant.format_hgvs("1", 35366, "C", "T"),
-                         'chr1:g.35366C>T')
-        self.assertEqual(myvariant.format_hgvs("chr2", 17142, "G", "GA"),
-                         'chr2:g.17142_17143insA')
-        self.assertEqual(myvariant.format_hgvs("1", 10019, "TA", "T"),
-                         'chr1:g.10020del')
-        self.assertEqual(myvariant.format_hgvs("MT", 8270, "CACCCCCTCT", "C"),
-                         'chrMT:g.8271_8279del')
-        self.assertEqual(myvariant.format_hgvs("7", 15903, "G", "GC"),
-                         'chr7:g.15903_15904insC')
-        self.assertEqual(myvariant.format_hgvs("X", 107930849, "GGA", "C"),
-                         'chrX:g.107930849_107930851delinsC')
-        self.assertEqual(myvariant.format_hgvs("20", 1234567, "GTC", "GTCT"),
-                         'chr20:g.1234569_1234570insT')
+        self.assertEqual(myvariant.format_hgvs("1", 35366, "C", "T"), "chr1:g.35366C>T")
+        self.assertEqual(
+            myvariant.format_hgvs("chr2", 17142, "G", "GA"), "chr2:g.17142_17143insA"
+        )
+        self.assertEqual(
+            myvariant.format_hgvs("1", 10019, "TA", "T"), "chr1:g.10020del"
+        )
+        self.assertEqual(
+            myvariant.format_hgvs("MT", 8270, "CACCCCCTCT", "C"), "chrMT:g.8271_8279del"
+        )
+        self.assertEqual(
+            myvariant.format_hgvs("7", 15903, "G", "GC"), "chr7:g.15903_15904insC"
+        )
+        self.assertEqual(
+            myvariant.format_hgvs("X", 107930849, "GGA", "C"),
+            "chrX:g.107930849_107930851delinsC",
+        )
+        self.assertEqual(
+            myvariant.format_hgvs("20", 1234567, "GTC", "GTCT"),
+            "chr20:g.1234569_1234570insT",
+        )
 
     def test_metadata(self):
         meta = self.mv.metadata()
         self.assertTrue("stats" in meta)
-        self.assertTrue("total" in meta['stats'])
+        self.assertTrue("total" in meta["stats"])
 
     def test_getvariant(self):
         v = self.mv.getvariant("chr9:g.107620835G>A")
-        self.assertEqual(v['_id'], "chr9:g.107620835G>A")
-        self.assertEqual(v['snpeff']['ann']['genename'], 'ABCA1')
+        self.assertEqual(v["_id"], "chr9:g.107620835G>A")
+        self.assertEqual(v["snpeff"]["ann"]["genename"], "ABCA1")
 
-        v = self.mv.getvariant("'chr1:g.1A>C'")   # something does not exist
+        v = self.mv.getvariant("'chr1:g.1A>C'")  # something does not exist
         self.assertEqual(v, None)
 
     def test_getvariant_with_fields(self):
         v = self.mv.getvariant("chr9:g.107620835G>A", fields="dbnsfp,cadd,cosmic")
-        self.assertTrue('_id' in v)
-        self.assertTrue('dbnsfp' in v)
-        self.assertTrue('cadd' in v)
-        self.assertTrue('cosmic' in v)
+        self.assertTrue("_id" in v)
+        self.assertTrue("dbnsfp" in v)
+        self.assertTrue("cadd" in v)
+        self.assertTrue("cosmic" in v)
 
     def test_getvariants(self):
         v_li = self.mv.getvariants(self.query_list1)
         self.assertEqual(len(v_li), 9)
-        self.assertEqual(v_li[0]['_id'], self.query_list1[0])
-        self.assertEqual(v_li[1]['_id'], self.query_list1[1])
-        self.assertEqual(v_li[2]['_id'], self.query_list1[2])
+        self.assertEqual(v_li[0]["_id"], self.query_list1[0])
+        self.assertEqual(v_li[1]["_id"], self.query_list1[1])
+        self.assertEqual(v_li[2]["_id"], self.query_list1[2])
 
         self.mv.step = 4
         # test input is a string of comma-separated ids
-        v_li2 = self.mv.getvariants(','.join(self.query_list1))
+        v_li2 = self.mv.getvariants(",".join(self.query_list1))
         self.assertEqual(v_li, v_li2)
         # test input is a tuple
         v_li2 = self.mv.getvariants(tuple(self.query_list1))
@@ -104,47 +120,48 @@ class TestMyVariantPy(unittest.TestCase):
         def _input(li):
             for x in li:
                 yield x
+
         v_li2 = self.mv.getvariants(_input(self.query_list1))
         self.assertEqual(v_li, v_li2)
         self.mv.step = 1000
 
     def test_query(self):
-        qres = self.mv.query('dbnsfp.genename:cdk2', size=5)
-        self.assertTrue('hits' in qres)
-        self.assertEqual(len(qres['hits']), 5)
+        qres = self.mv.query("dbnsfp.genename:cdk2", size=5)
+        self.assertTrue("hits" in qres)
+        self.assertEqual(len(qres["hits"]), 5)
 
     def test_query_hgvs(self):
         qres = self.mv.query('"NM_000048.3:c.566A>G"', size=5)
-        self.assertTrue('hits' in qres)
-        self.assertEqual(len(qres['hits']), 1)
+        self.assertTrue("hits" in qres)
+        self.assertEqual(len(qres["hits"]), 1)
 
     def test_query_rsid(self):
-        qres = self.mv.query('dbsnp.rsid:rs58991260')
-        self.assertTrue('hits' in qres)
-        self.assertEqual(len(qres['hits']), 1)
-        self.assertEqual(qres['hits'][0]['_id'], 'chr1:g.218631822G>A')
-        qres2 = self.mv.query('rs58991260')
+        qres = self.mv.query("dbsnp.rsid:rs58991260")
+        self.assertTrue("hits" in qres)
+        self.assertEqual(len(qres["hits"]), 1)
+        self.assertEqual(qres["hits"][0]["_id"], "chr1:g.218631822G>A")
+        qres2 = self.mv.query("rs58991260")
         # exclude _score field before comparison
-        qres['hits'][0].pop('_score')
-        qres2['hits'][0].pop('_score')
-        self.assertEqual(qres['hits'], qres2['hits'])
+        qres["hits"][0].pop("_score")
+        qres2["hits"][0].pop("_score")
+        self.assertEqual(qres["hits"], qres2["hits"])
 
     def test_query_symbol(self):
-        qres = self.mv.query('snpeff.ann.genename:cdk2')
-        self.assertTrue('hits' in qres)
-        self.assertTrue(qres['total'] > 5000)
-        self.assertEqual(qres['hits'][0]['snpeff']['ann'][0]['genename'], 'CDK2')
+        qres = self.mv.query("snpeff.ann.genename:cdk2")
+        self.assertTrue("hits" in qres)
+        self.assertTrue(qres["total"] > 5000)
+        self.assertEqual(qres["hits"][0]["snpeff"]["ann"][0]["genename"], "CDK2")
 
     def test_query_genomic_range(self):
-        qres = self.mv.query('chr1:69000-70000')
-        self.assertTrue('hits' in qres)
-        self.assertTrue(qres['total'] >= 3)
+        qres = self.mv.query("chr1:69000-70000")
+        self.assertTrue("hits" in qres)
+        self.assertTrue(qres["total"] >= 3)
 
     def test_query_fetch_all(self):
-        qres = self.mv.query('chr1:69500-70000', fields="chrom")
-        total = qres['total']
+        qres = self.mv.query("chr1:69500-70000", fields="chrom")
+        total = qres["total"]
 
-        qres = self.mv.query('chr1:69500-70000', fields="chrom", fetch_all=True)
+        qres = self.mv.query("chr1:69500-70000", fields="chrom", fetch_all=True)
         self.assertTrue(isinstance(qres, types.GeneratorType))
         self.assertEqual(total, len(list(qres)))
 
@@ -154,7 +171,7 @@ class TestMyVariantPy(unittest.TestCase):
 
         self.mv.step = 4
         # test input as a string
-        qres2 = self.mv.querymany(','.join(self.query_list1), verbose=False)
+        qres2 = self.mv.querymany(",".join(self.query_list1), verbose=False)
         self.assertEqual(qres, qres2)
         # test input as a tuple
         qres2 = self.mv.querymany(tuple(self.query_list1), verbose=False)
@@ -165,52 +182,81 @@ class TestMyVariantPy(unittest.TestCase):
         self.mv.step = 1000
 
     def test_querymany_with_scopes(self):
-        qres = self.mv.querymany(['rs58991260', 'rs2500'], scopes='dbsnp.rsid', verbose=False)
+        qres = self.mv.querymany(
+            ["rs58991260", "rs2500"], scopes="dbsnp.rsid", verbose=False
+        )
         self.assertEqual(len(qres), 2)
 
-        qres = self.mv.querymany(['RCV000083620', 'RCV000083611', 'RCV000083584'], scopes='clinvar.rcv_accession', verbose=False)
+        qres = self.mv.querymany(
+            ["RCV000083620", "RCV000083611", "RCV000083584"],
+            scopes="clinvar.rcv_accession",
+            verbose=False,
+        )
         self.assertEqual(len(qres), 3)
 
-        qres = self.mv.querymany(['rs2500', 'RCV000083611', 'COSM1392449'],
-                                 scopes='clinvar.rcv_accession,dbsnp.rsid,cosmic.cosmic_id', verbose=False)
+        qres = self.mv.querymany(
+            ["rs2500", "RCV000083611", "COSM1392449"],
+            scopes="clinvar.rcv_accession,dbsnp.rsid,cosmic.cosmic_id",
+            verbose=False,
+        )
         self.assertEqual(len(qres), 3)
 
     def test_querymany_fields(self):
-        ids = ['COSM1362966', 'COSM990046', 'COSM1392449']
-        qres1 = self.mv.querymany(ids, scopes='cosmic.cosmic_id', fields=['cosmic.tumor_site', 'cosmic.cosmic_id'], verbose=False)
+        ids = ["COSM1362966", "COSM990046", "COSM1392449"]
+        qres1 = self.mv.querymany(
+            ids,
+            scopes="cosmic.cosmic_id",
+            fields=["cosmic.tumor_site", "cosmic.cosmic_id"],
+            verbose=False,
+        )
         self.assertEqual(len(qres1), 3)
 
-        qres2 = self.mv.querymany(ids, scopes='cosmic.cosmic_id', fields='cosmic.tumor_site,cosmic.cosmic_id', verbose=False)
+        qres2 = self.mv.querymany(
+            ids,
+            scopes="cosmic.cosmic_id",
+            fields="cosmic.tumor_site,cosmic.cosmic_id",
+            verbose=False,
+        )
         self.assertEqual(len(qres2), 3)
 
         self.assertEqual(qres1, qres2)
 
     def test_querymany_notfound(self):
-        qres = self.mv.querymany(['rs58991260', 'rs2500', 'NA_TEST'], scopes='dbsnp.rsid', verbose=False)
+        qres = self.mv.querymany(
+            ["rs58991260", "rs2500", "NA_TEST"], scopes="dbsnp.rsid", verbose=False
+        )
         self.assertEqual(len(qres), 3)
-        self.assertEqual(qres[2], {"query": 'NA_TEST', "notfound": True})
+        self.assertEqual(qres[2], {"query": "NA_TEST", "notfound": True})
 
     def test_querymany_dataframe(self):
         if not pandas_avail:
             from nose.plugins.skip import SkipTest
+
             raise SkipTest
-        qres = self.mv.querymany(self.query_list2, scopes='dbsnp.rsid', fields='dbsnp', as_dataframe=True, verbose=False)
+        qres = self.mv.querymany(
+            self.query_list2,
+            scopes="dbsnp.rsid",
+            fields="dbsnp",
+            as_dataframe=True,
+            verbose=False,
+        )
         self.assertTrue(isinstance(qres, DataFrame))
-        self.assertTrue('dbsnp.vartype' in qres.columns)
+        self.assertTrue("dbsnp.vartype" in qres.columns)
         self.assertEqual(set(self.query_list2), set(qres.index))
 
     def test_querymany_step(self):
-        qres1 = self.mv.querymany(self.query_list2, scopes='dbsnp.rsid', verbose=False)
+        qres1 = self.mv.querymany(self.query_list2, scopes="dbsnp.rsid", verbose=False)
         default_step = self.mv.step
         self.mv.step = 3
-        qres2 = self.mv.querymany(self.query_list2, scopes='dbsnp.rsid', verbose=False)
+        qres2 = self.mv.querymany(self.query_list2, scopes="dbsnp.rsid", verbose=False)
         self.mv.step = default_step
         self.assertEqual(qres1, qres2)
 
     def test_get_fields(self):
         fields = self.mv.get_fields()
-        self.assertTrue('dbsnp' in fields.keys())
-        self.assertTrue('clinvar' in fields.keys())
+        self.assertTrue("dbsnp" in fields.keys())
+        self.assertTrue("clinvar" in fields.keys())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
